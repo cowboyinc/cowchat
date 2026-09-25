68732411efe3e4e30e3ab76a58d3197bb17deb47
Logan <logan@cowboy.inc>
2026-09-25 13:47:16 +0000

    Consume Cowchat client crates from cowboy-protocol

--- crates/cowchat-client/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod connection;
-#[cfg(feature = "member")]
-pub mod member;
-
-pub use connection::{ActorReply, ClientError, CowchatClient, Event};
--- crates/cowchat-core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod crypto;
-pub mod error;
-pub mod models;
-pub mod protocol;
-pub mod room_crypto;
-
-pub use error::{ErrorCode, ErrorPayload};
-pub use models::*;
-pub use protocol::{Frame, FrameType, MIN_SUPPORTED_PROTOCOL, PROTOCOL_VERSION};
